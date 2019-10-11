152690c894cfb89ba63bc4ac73679e34f24e1d65
Dawdu <dawduwrites@gmail.com>
2019-10-11 19:41:30 -0400

    fixed value function

--- hazyCalculator.js
+++ hazyCalculator.js
@@ -1,18 +1,20 @@
 function isSkippedValue(value) {
-  return !value
-}
+ if (value == "" || value === undefined)   return true 
+return false 
+} 
+
 
 function isNumericValue(value) {
-  return !isNaN(value)
+  return !isNaN(value) && value !== ""
 }
 
 function isNothingValue(value) {
-  return value === null
+  return value == null
 }
 
 function isAcceptableValue(value) {
   const operators = ['+', '-', '*', '/']
-  return typeof value === Number || operators.includes(value)
+  return typeof Number(value) === 'number' || operators.includes(value)
 }
 
 function performCalculationStep(firstOperand, operator, secondOperand) {
